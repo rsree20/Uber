3ee908f0f8c36b7bb04da458e044fde4502c69cc
Ravali <Ravali@DESKTOP-CE3CCQ5>
2018-12-01 10:35:13 +0530

    aesfasd

--- src/main/java/com/uber/employeePojos/User.java
+++ src/main/java/com/uber/employeePojos/User.java
@@ -2,4 +2,14 @@ package com.uber.employeePojos;
 
 public class User {
 
+	private int userId;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
 }
