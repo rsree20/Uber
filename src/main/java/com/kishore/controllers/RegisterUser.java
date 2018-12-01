e3d0649b7fb000f217ed6aeabd2893a9588fb9b1
kishore <kishore@kishore-PC>
2018-12-01 12:40:43 +0530

    Registration validation implemented

---
+++ src/main/java/com/kishore/controllers/RegisterUser.java
@@ -0,0 +1,8 @@
+package com.kishore.controllers;
+
+public class RegisterUser {
+	public static void main(String[] args) {
+		System.out.println("Welcome food lovers!! please register here for biryani");
+	}
+
+}
