e148e59b4927e11304b534e3f08c752c2183ca9a
deepika <deepika@deepika-PC>
2018-12-01 03:13:14 -0800

    implemented java checks

---
+++ src/main/java/com/rama/controller/rama1.java
@@ -0,0 +1,12 @@
+package com.rama.controller;
+
+public class rama1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Ram");
+		System.out.println("mahesh Babu");
+		System.out.println("Prabash");
+		
+	}
+
+}
