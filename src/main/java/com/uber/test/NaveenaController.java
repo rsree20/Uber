29dab2c710af0ba31ee47a55eb49dafdb6a7afc5
rajesh k <rajesh k@idea-PC>
2018-12-01 12:40:18 +0530

    naveena Done

--- src/main/java/com/uber/test/NaveenaController.java
+++ src/main/java/com/uber/test/NaveenaController.java
@@ -2,7 +2,7 @@ package com.uber.test;
 
 public class NaveenaController {
 	public static void main(String[] args) {
-		System.out.println(" Kuntalabavi naveena");
+		System.out.println(" Kuntalabavi naveena");  
 	}
 
 }
