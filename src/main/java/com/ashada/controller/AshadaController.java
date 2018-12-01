665d99e027b26487567b2a7b8c7408ae218c6da8
Admin <Admin@DESKTOP-OEBRPIM>
2018-12-01 12:37:26 +0530

    hello

---
+++ src/main/java/com/ashada/controller/AshadaController.java
@@ -0,0 +1,13 @@
+package com.ashada.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AshadaController {
+	
+	public static void main(String[] args){
+		
+		System.out.println(" hello Mahesh Babu");
+	}
+
+}
