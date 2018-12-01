acdcf92a096abb426b544f3f3bba69d25a3dde43
SREE <SREE@DESKTOP-OHJSHRN>
2018-12-01 11:37:36 +0530

    implemented security checks

---
+++ src/main/java/com/UberEats/SecurityController.java
@@ -0,0 +1,11 @@
+package com.UberEats;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SecurityController {
+
+	public void securityCheck() {
+		System.out.println("Helo!! Am doing security check!!");
+	}
+}
