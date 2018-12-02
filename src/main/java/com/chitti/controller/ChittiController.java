99a403034fe354c21bcaa876aac8862afd28455c
Indian <Indian@DESKTOP-CT5D6NU>
2018-12-02 11:55:42 +0530

    update Chitti

---
+++ src/main/java/com/chitti/controller/ChittiController.java
@@ -0,0 +1,13 @@
+package com.chitti.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChittiController {
+	public static void main(String[] args) {
+		System.out.println("ravi");
+		System.out.println("raju");
+	}
+	
+
+}
