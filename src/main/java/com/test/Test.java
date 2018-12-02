3900b37dc352c50ecf6085eb45a8eee1d0c1dfc3
Srinivas <Srinivas@DESKTOP-J7EPG5A>
2018-12-02 12:50:27 +0530

    srinu43 removed from sysout

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -3,7 +3,7 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-System.out.println("testing new branch srinu43");
+System.out.println("testing new branch");
 	}
 
 }
