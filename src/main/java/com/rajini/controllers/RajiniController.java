c238f152b694abc56229685043ab4b4e2cf5993e
Indian <Indian@DESKTOP-CT5D6NU>
2018-12-02 12:01:10 +0530

    rajini controller

---
+++ src/main/java/com/rajini/controllers/RajiniController.java
@@ -0,0 +1,11 @@
+package com.rajini.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RajiniController {
+public static void main(String[] args) {
+	System.out.println("rajini");
+	System.out.println("sruthi");
+}
+}
