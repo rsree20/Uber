ef3427014aa8cdafcdd8723e63e54d3d9e548edd
Admin <Admin@DESKTOP-OEBRPIM>
2018-12-02 11:51:14 +0530

    hello

---
+++ src/main/java/com/hero/controller/HeroController.java
@@ -0,0 +1,12 @@
+package com.hero.controller;
+
+
+public class HeroController {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(" entered into the location...");
+		
+	}
+
+}
