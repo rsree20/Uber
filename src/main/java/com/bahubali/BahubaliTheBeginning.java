c52fe5d638adb1bab96bab76fa5f6d6399f94f7e
Venkat Yadav .V <Venkat Yadav .V@LAPTOP-59NQRCIM>
2018-12-04 11:03:22 +0530

    adition done

--- src/main/java/com/bahubali/BahubaliTheBeginning.java
+++ src/main/java/com/bahubali/BahubaliTheBeginning.java
@@ -1,6 +1,7 @@
 package com.bahubali;
 
 public class BahubaliTheBeginning {
+	int c;
 public static void main(String[] args) {
 	System.out.println("Welcome to bahubali unit....");
 	
@@ -8,7 +9,8 @@ public static void main(String[] args) {
 public int addition(int a,int b) {
 	a=10;
 	b=20;
-	return a+b;
-	
+	c=a+b;
+	System.out.println("Addition of a and b is:"+c);
+	return c;	
 }
 }
