046cdd21f76fe950accc04c4a4d257c89adf8c9a
Venkat Yadav .V <Venkat Yadav .V@LAPTOP-59NQRCIM>
2018-12-04 10:50:05 +0530

    addtion is committed....

--- src/main/java/com/bahubali/BahubaliTheBeginning.java
+++ src/main/java/com/bahubali/BahubaliTheBeginning.java
@@ -3,5 +3,12 @@ package com.bahubali;
 public class BahubaliTheBeginning {
 public static void main(String[] args) {
 	System.out.println("Welcome to bahubali unit....");
+	
+}
+public int addition(int a,int b) {
+	a=10;
+	b=20;
+	return a+b;
+	
 }
 }
