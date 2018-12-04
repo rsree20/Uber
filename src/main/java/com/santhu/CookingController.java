8a5538518900d89676b26d8bf5067bfdb63074df
user <user@user-PC>
2018-12-04 12:46:35 +0530

    mobile First done

--- src/main/java/com/santhu/CookingController.java
+++ src/main/java/com/santhu/CookingController.java
@@ -2,7 +2,7 @@ package com.santhu;
 
 public class CookingController {
 	public static void main(String[] args) {
-		System.out.println("helo cooking food");
+		System.out.println("mobile first");
 	}
 
 }
