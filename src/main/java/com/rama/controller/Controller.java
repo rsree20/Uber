2fa01ad83de0fb2e188afe530cd10c239da3c7e4
deepika <deepika@deepika-PC>
2018-12-04 01:19:15 -0800

    hii

--- src/main/java/com/rama/controller/Controller.java
+++ src/main/java/com/rama/controller/Controller.java
@@ -1,5 +1,12 @@
 package com.rama.controller;
 
 public class Controller {
-
+  public static void main(String [] args)
+  {
+	  String name="devi";
+	  Validate v=new Validate();
+	  System.out.println(v.checkEmpty(name));
+	  System.out.println("Controller implemented");  
+	  
+  }
 }
