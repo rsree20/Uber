43d2050060da705bdc0a6a37fa01f83289c02e88
user <user@user-PC>
2018-12-04 11:04:25 +0530

    forloop logic implemented

--- src/main/java/com/logic/Logic.java
+++ src/main/java/com/logic/Logic.java
@@ -3,6 +3,10 @@ package com.logic;
 public class Logic {
 	public static void main(String[] args) {
 		 System.out.println("logic");
+		 for (int i = 0; i < 10; i++) {
+			 System.out.println(i);
+			
+		}
 	}
 
 }
