c693edf81cbfd2253d1f41656478cb8903cf5f88
SREE <SREE@DESKTOP-OHJSHRN>
2018-12-04 08:13:43 +0530

    done with review comments

--- src/main/java/com/comments/CommentsController.java
+++ src/main/java/com/comments/CommentsController.java
@@ -6,6 +6,5 @@ public class CommentsController {
 		String name = "raju";
 		Validation val = new Validation();
 		System.out.println(val.checkEmpty(name));
-		System.out.println("Comments implemented here!!");
 	}
 }
