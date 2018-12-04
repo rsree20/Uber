46837714ca2895301161c8fcff039c2f215b0e1a
Ravali <Ravali@DESKTOP-CE3CCQ5>
2018-12-04 08:10:49 +0530

    worked on review comments

--- src/main/java/com/likecontroller/Likevalidation.java
+++ src/main/java/com/likecontroller/Likevalidation.java
@@ -2,7 +2,9 @@ package com.likecontroller;
 
 public class Likevalidation {
 public static void main(String[] args) {
+	System.out.println("validation done");
 	System.out.println("like ");
+	
 }
 
 }
