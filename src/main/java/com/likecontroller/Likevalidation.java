ca9ecf1d94d3cafc49a33ffa91a86d1d223dd3a2
Ravali <Ravali@DESKTOP-CE3CCQ5>
2018-12-04 08:36:50 +0530

    h

--- src/main/java/com/likecontroller/Likevalidation.java
+++ src/main/java/com/likecontroller/Likevalidation.java
@@ -3,6 +3,7 @@ package com.likecontroller;
 public class Likevalidation {
 public static void main(String[] args) {
 	System.out.println("validation done");
+	
 	System.out.println("like ");
 	System.out.println("hi");
 	System.out.println("hello");
