45dba3abac8d244977774fe8d914a6542d74824b
SREE <SREE@DESKTOP-OHJSHRN>
2018-12-05 08:32:37 +0530

    klajf;lkdsafj

--- src/main/java/com/anil/controllers/AnilController.java
+++ src/main/java/com/anil/controllers/AnilController.java
@@ -3,5 +3,6 @@ package com.anil.controllers;
 public class AnilController {
 	public static void main(String[] args) {
 		System.out.println("Hi, now its my time ");
+		
 	}
 }
--- src/main/java/com/dbutil/Test240.java
+++ src/main/java/com/dbutil/Test240.java
@@ -3,5 +3,6 @@ package com.dbutil;
 public class Test240 {
 public static void main(String[] args) {
 	System.out.println(240);
+	System.out.println("Helo");
 }
 }
