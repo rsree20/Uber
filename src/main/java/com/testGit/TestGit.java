197c8af128ee81ccc83622a300647a80617b70ec
user <user@admin>
2018-12-05 09:16:37 +0530

    method added

--- src/main/java/com/testGit/TestGit.java
+++ src/main/java/com/testGit/TestGit.java
@@ -6,5 +6,9 @@ public class TestGit {
 		System.out.println("welcome to TestGit");
 		System.out.println("ended");
 	}
+	public void isMailValid() {
+		
+		System.out.println("entered into TestGit::isValidMail ");
+	}
 
 }
